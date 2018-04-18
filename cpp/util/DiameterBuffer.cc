896cde8723300ca7d1ca5d738b89744401182c1c
Erik Berglund <erik.berglund@jeppesen.com>
2018-04-18 12:29:10 +0200

    Added C++ implementation of DiameterBuffer.

---
+++ cpp/util/DiameterBuffer.cc
@@ -0,0 +1,46 @@
+/**
+ * Created by Erik Berglund on 2018-04-18.
+ */
+
+#include <limits>
+#include "DiameterBuffer.h"
+
+
+void DiameterBuffer::updateBuffer(vector<double> data)
+{
+  // calculate the distance from the new input to all inputs in the buffer
+  // find the largest distance between the input and any entry in the buffer
+  // as well as the entry in the buffer that is closest to the input
+  int minDistIndex = 0;
+  double minDist = std::numeric_limits<double>::max();
+  double maxNewDist = 0;
+  for(int index = 0;index<buffer.size();index++)
+  {
+    vector<double> &n = buffer.at(index);
+    double tmp = bufferMetric.getDistance(data,n);
+    if(tmp < minDist)
+    {
+      // Closest buffer entry so far.
+      minDist = tmp;
+      minDistIndex = index;
+    }
+    if(tmp > maxNewDist)
+    {
+      // Largest distance so far.
+      maxNewDist = tmp;
+    }
+  }
+  // Check if we've received a 'distant' input
+  if(maxNewDist > maxDiameter)
+  {
+    // Add the new input to the buffer
+    maxDiameter = maxNewDist;
+    buffer.push_back(data);
+    // Keep the buffer to the set size...
+    if(buffer.size() > (data.size()+1))
+    {
+      // ... by removing the buffer entry that is closest to the new input
+      buffer.erase(buffer.begin()+minDistIndex);
+    }
+  }
+}
---
+++ cpp/util/DiameterBuffer.h
@@ -0,0 +1,34 @@
+/**
+ * Created by Erik Berglund on 2018-04-18.
+ */
+
+#ifndef CPP_DIAMETERBUFFER_H
+#define CPP_DIAMETERBUFFER_H
+
+#include <vector>
+#include "../metrics/EuclideanMetric.h"
+
+using namespace std;
+
+class DiameterBuffer
+{
+private:
+  vector<vector<double>> buffer;
+  EuclideanMetric bufferMetric;
+  double maxDiameter = -1;
+
+public:
+  double getMaxDiameter()
+  {
+    return maxDiameter;
+  }
+  void setMaxDiameter(double newDiameter)
+  {
+    maxDiameter = newDiameter;
+  }
+
+  void updateBuffer(vector<double> data);
+};
+
+
+#endif //CPP_DIAMETERBUFFER_H
