89904760ba33690f74effa245129562427e66efa
erikjber <erik.berglund@gmail.com>
2020-02-11 09:28:35 +0100

    Minor changes in naming and accessibility.

--- java/src/main/java/org/plsomlib/recursive/MulitlayerRecursivePLSOM2.java
+++ java/src/main/java/org/plsomlib/recursive/MulitlayerRecursivePLSOM2.java
@@ -14,7 +14,7 @@ import org.plsomlib.util.IterativeArray;
  * @author Erik Berglund
  *
  */
-public class NewMulitlayerRecursivePLSOM2
+public class MulitlayerRecursivePLSOM2
 {
     // Weights 
     protected IterativeArray<double[]> directWeights;
@@ -22,16 +22,16 @@ public class NewMulitlayerRecursivePLSOM2
     protected IterativeArray<double[]> feedbackWeights;
     
     // Diameter buffers
-    protected DiameterBuffer directDiameterBuffer = new DiameterBuffer();
-    protected DiameterBuffer selfDiameterBuffer = new DiameterBuffer();
-    protected DiameterBuffer feedbackDiameterBuffer = new DiameterBuffer();
+    protected DiameterBuffer directDiameterBuffer;
+    protected DiameterBuffer selfDiameterBuffer;
+    protected DiameterBuffer feedbackDiameterBuffer;
     
     protected double [] excitations;
     protected double [] nuExcitations;
     
     // How much weight to give to the direct weights relative to the selfWeights+feedbackWeights;
     private double alpha;
-    private NewMulitlayerRecursivePLSOM2 otherNet;
+    private MulitlayerRecursivePLSOM2 otherNet;
     private int[] outputDimensions;
     
     protected Random rand = new Random();
@@ -40,7 +40,7 @@ public class NewMulitlayerRecursivePLSOM2
     private double lastError;
     private int[] winner;
     
-    private Metric inputMetric = new SquaredEuclideanMetric();
+    private Metric inputMetric = new EuclideanMetric();
     private Metric outputMetric = new EuclideanMetric();
     private NeighbourhoodFunction nhFunction = new GaussianNeighbourhoodFunction();
     private boolean predict;
@@ -53,7 +53,7 @@ public class NewMulitlayerRecursivePLSOM2
     /**
      * 
      */
-    public NewMulitlayerRecursivePLSOM2( double alpha,int inputDim, double neighbourhoodRange, int ... dimensions)
+    public MulitlayerRecursivePLSOM2( double alpha,int inputDim, double neighbourhoodRange, int ... dimensions)
     {
         this.alpha = alpha;
         this.outputDimensions = dimensions;
@@ -66,7 +66,19 @@ public class NewMulitlayerRecursivePLSOM2
         excitations = new double[directWeights.getCount()];
         nuExcitations = new double[directWeights.getCount()];
         
+
+        directDiameterBuffer = new DiameterBuffer();
+        selfDiameterBuffer = new DiameterBuffer();
+        feedbackDiameterBuffer = new DiameterBuffer();
+        
         initWeights();
+    }
+    
+    /**
+     * Used for cloning.
+     */
+    private MulitlayerRecursivePLSOM2()
+    {
     }
     
     public int [] getOutputDimensions()
@@ -109,7 +121,7 @@ public class NewMulitlayerRecursivePLSOM2
     protected void initFeedbackWeights()
     {
         int nodeCount = otherNet.directWeights.toArray().length;
-        for (int x = 0; x < this.feedbackWeights.toArray().length; x++)
+        for (int x = 0; x < this.directWeights.toArray().length; x++)
         {
             double[] tmpArray = new double[nodeCount];
             // initialize weight to random values
@@ -121,12 +133,23 @@ public class NewMulitlayerRecursivePLSOM2
         }
     }
     
-    public void setOtherNet(NewMulitlayerRecursivePLSOM2 other)
+    /**
+     * Set the next network in a multinetwork and create and initialise the weights.
+     */
+    public void setOtherNet(MulitlayerRecursivePLSOM2 other)
     {
         this.otherNet = other;
-        feedbackWeights = new IterativeArray<>(other.getOutputDimensions());
+        feedbackWeights = new IterativeArray<>(getOutputDimensions());
         initFeedbackWeights();
     }
+    /**
+     * Set the next network in a multinetwork without creating an initialising weights.
+     */
+    public void setOtherNetManual(MulitlayerRecursivePLSOM2 other)
+    {
+        this.otherNet = other;
+    }
+    
     
     public double [] getExcitations()
     {
@@ -144,7 +167,7 @@ public class NewMulitlayerRecursivePLSOM2
     public void train(double [] input)
     {
         setInput(input);
-        setWinner(classify());
+        classify();
         // calculate epsilon
         if (getLastError() == 0)
         {
@@ -170,27 +193,28 @@ public class NewMulitlayerRecursivePLSOM2
     protected void updateWeights()
     {
         Object[] data = directWeights.toArray();
-        for (int x = 0; x < data.length; x++)
+        int winnerIndex = directWeights.getOffset(getWinner());
+        for (int nodeIndex = 0; nodeIndex < data.length; nodeIndex++)
         {
             // calculate the neighbourhood scaling, multiply by epsilon
-            double anhc = getEpsilon() * getNeighbourhoodScaling(directWeights.getPosition(x), getWinner(), getNeighbourhoodSize());
+            double anhc = getEpsilon() * getNeighbourhoodScaling(directWeights.getPosition(nodeIndex), getWinner(), getNeighbourhoodSize());
             if(!isPredict())
             {
-                double[] weight = (double[]) data[x];
+                double[] weight = (double[]) data[nodeIndex];
                 // update the weights
                 for (int wIndex = 0; wIndex < weight.length; wIndex++)
                 {
                     weight[wIndex] += anhc * (input[wIndex] - weight[wIndex]);
                 }
             }
-            double[] selfW = selfWeights.getValueFromOffset(x);
+            double[] selfW = selfWeights.getValueFromOffset(nodeIndex);
             for (int wIndex = 0; wIndex < selfW.length; wIndex++)
             {
                 selfW[wIndex] += anhc * (excitations[wIndex] - selfW[wIndex]);
             }
             if(this.feedbackWeights != null)
             {
-                double[] feedbackW = feedbackWeights.getValueFromOffset(x);
+                double[] feedbackW = feedbackWeights.getValueFromOffset(nodeIndex);
                 for (int wIndex = 0; wIndex < feedbackW.length; wIndex++)
                 {
                     feedbackW[wIndex] += anhc * (otherNet.getExcitations()[wIndex] - feedbackW[wIndex]);
@@ -265,7 +289,7 @@ public class NewMulitlayerRecursivePLSOM2
         this.winner = res;
     }
     
-    private int [] getWinner()
+    public int [] getWinner()
     {
         return this.winner;
     }
@@ -287,6 +311,8 @@ public class NewMulitlayerRecursivePLSOM2
     protected double findMinDist( int[] res)
     {
         double minDist = Double.POSITIVE_INFINITY;
+        double minExcitation = 1;
+        double maxExcitation = 0;
    
         double direct = alpha;
         double indirect = 1-alpha;
@@ -302,39 +328,49 @@ public class NewMulitlayerRecursivePLSOM2
         double totalExpExitation = 0;
         int winner = 0;
         Object [] directWeightsArray = directWeights.toArray();
-        for(int x = 0;x<directWeightsArray.length;x++)
+        for(int nodeIndex = 0;nodeIndex<directWeightsArray.length;nodeIndex++)
         {
-            double [] directW = (double[])directWeightsArray[x];
-            double [] selfW = (double[])selfWeights.getValueFromOffset(x);
-            double [] feedbackW = null;
+            double [] directW = (double[])directWeightsArray[nodeIndex];
+            double [] selfW = (double[])selfWeights.getValueFromOffset(nodeIndex);
+            double dist = inputMetric.getDistance(selfW, getExcitations())*indirect;
             if(this.feedbackWeights != null)
             {
-                feedbackW = (double[])feedbackWeights.getValueFromOffset(x);
+                double [] feedbackW = null;
+                feedbackW = (double[])feedbackWeights.getValueFromOffset(nodeIndex);
+                dist += inputMetric.getDistance(feedbackW, otherNet.getExcitations())*indirect;
             }
-            
-            double dist = inputMetric.getDistance(selfW, getExcitations())*indirect;
             if(!isPredict())
             {
                 dist += inputMetric.getDistance(directW, input)*direct;
             }
-            if(this.feedbackWeights!=null)
-            {
-                dist += inputMetric.getDistance(feedbackW, otherNet.getExcitations())*indirect;
-            }
             double excitation = Math.exp(-dist);
-            nuExcitations[x] = excitation;
-            totalExpExitation += Math.exp(excitation);
+            nuExcitations[nodeIndex] = excitation;
             if(dist<minDist)
             {
                 minDist = dist;
-                winner = x;
+                winner = nodeIndex;
             }
+            maxExcitation = Math.max(excitation, maxExcitation);
+            minExcitation = Math.min(excitation, minExcitation);
+            
         }
+    
         System.arraycopy(directWeights.getPosition(winner),0,res,0,res.length);
+    
+        double diff = maxExcitation - minExcitation;
+        if (diff <= 0)
+        {
+            // all excitations are equal
+            diff = 1;
+        }
+
+        // normalise excitations
         for (int node = 0; node < nuExcitations.length; node++)
         {
-            nuExcitations[node] = Math.exp(nuExcitations[node])/totalExpExitation;
+            nuExcitations[node] -= minExcitation;
+            nuExcitations[node] /= diff;
         }
+      
         return minDist;
     }
     
@@ -365,6 +401,67 @@ public class NewMulitlayerRecursivePLSOM2
     public void train()
     {
         train(null);
+    }
+
+    /**
+     * Get the input weights for the given node.
+     */
+    public double[] getInputWeights(int[] node)
+    {
+        return directWeights.getValue(node);
+    }
+
+    /**
+     * A deep copy.
+     * The otherNet member variable must be set outside this method.
+     * @see java.lang.Object#clone()
+     */
+    @Override
+    public Object clone() throws CloneNotSupportedException
+    {
+        MulitlayerRecursivePLSOM2 res = new MulitlayerRecursivePLSOM2();
+        res.alpha = alpha;
+        res.directDiameterBuffer = (DiameterBuffer) directDiameterBuffer.clone();
+        res.directWeights = (IterativeArray<double[]>) directWeights.clone();
+        res.epsilon = epsilon;
+        res.excitations = excitations.clone();
+        res.feedbackDiameterBuffer = (DiameterBuffer) feedbackDiameterBuffer.clone();
+        if(feedbackWeights!=null)
+        {
+            res.feedbackWeights = (IterativeArray<double[]>) feedbackWeights.clone();
+        }
+        if(input != null)
+        {
+            res.input = input.clone();
+        }
+        res.inputDimension = inputDimension;
+        res.lastError = lastError;
+        res.neighbourhoodRange = neighbourhoodRange;
+        res.neighbourhoodSize = neighbourhoodSize;
+        res.nuExcitations = nuExcitations.clone();
+        res.outputDimensions = outputDimensions.clone();
+        res.predict = predict;
+        res.selfDiameterBuffer = (DiameterBuffer) selfDiameterBuffer.clone();
+        res.selfWeights = (IterativeArray<double[]>) selfWeights.clone();
+        if(winner != null)
+        {
+            res.winner = winner.clone();
+        }
+        return res;
+    }
+
+    /**
+     * @return 
+     * 
+     */
+    public IterativeArray<double[]> getInputWeights()
+    {
+        return directWeights;
+    }
+    
+    public IterativeArray<double[]> getRecurrentWeights()
+    {
+        return selfWeights;
     }
 
 }
